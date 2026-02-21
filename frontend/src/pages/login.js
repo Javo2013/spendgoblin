c39270f7dbfd19b77edefa53702e1b42a7e5e5fa
Javian Williams <javian.williams29@gmail.com>
2026-02-21 14:59:16 -0500

    Complete Login and Register pages

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://127.0.0.1:5000/login", {
+        username,
+        password,
+      });
+
+      onLogin(res.data.access_token);
+      navigate("/dashboard");
+    } catch (err) {
+      alert("Invalid credentials");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+
+      <p onClick={() => navigate("/register")}>
+        Create Account
+      </p>
+    </div>
+  );
+}
---
+++ frontend/src/pages/register.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://127.0.0.1:5000/register", {
+        username,
+        password,
+      });
+
+      navigate("/");
+    } catch (err) {
+      alert("User may already exist");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
